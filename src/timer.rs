9eaf9376e6d75802d07f8bc6b25160ca0f97e81a
ColorCookie <ColorCookie@protonmail.com>
2022-02-19 08:40:12 +0530

    Timer now also has an excess counter

--- src/timer.rs
+++ src/timer.rs
@@ -1,10 +1,14 @@
 use crate::counter::Counter;
+use crate::format::fmt_time;
 use crate::input::{listen_for_inputs, Command};
 use crate::notification::notify_default;
 use crate::sound::play_bell;
-use crate::terminal::{show_view, TermRawMode};
+use crate::terminal::{
+    clear, show_message, show_message_green, show_message_red, show_view, TermRawMode,
+};
 use anyhow::Result;
-use std::{sync::mpsc::Receiver, time::Instant};
+use std::{io::Stdout, sync::mpsc::Receiver, thread, time::Instant};
+use termion::raw::RawTerminal;
 
 pub struct Timer {
     started: Instant,
@@ -17,7 +21,9 @@ pub struct Timer {
 enum Status {
     Running,
     Paused,
-    Ended,
+    TimerEnd,       // When Timer expires
+    TimerEndPaused, // When Timer expires
+    Ended,          // Program End
 }
 
 impl Timer {
@@ -34,9 +40,55 @@ impl Timer {
         }
     }
 
-    fn alert(&self) -> Result<()> {
-        notify_default("You break ended!", "Time for some work")?;
-        play_bell()?;
+    fn alert(&self) {
+        thread::spawn(move || {
+            notify_default("You break ended!", "Time for some work").unwrap();
+            play_bell().unwrap();
+        });
+    }
+
+    fn end_timer(&mut self) {
+        self.pause();
+        self.status = Status::TimerEnd;
+    }
+
+    fn counter_now(&self) -> u64 {
+        let elapsed = self.started.elapsed().as_secs();
+        if self.counter > elapsed {
+            self.counter - elapsed
+        } else {
+            0
+        }
+    }
+
+    fn get_mut_stdout(&mut self) -> &mut RawTerminal<Stdout> {
+        &mut self.stdout_raw.stdout
+    }
+
+    fn show_extended_time(&mut self) -> Result<()> {
+        let counter = self.counter();
+        match self.status {
+            Status::TimerEnd => {
+                show_message_green(self.get_mut_stdout(), &format!("-{}", fmt_time(counter)), 2)?
+            }
+            Status::TimerEndPaused => {
+                show_message_red(self.get_mut_stdout(), &format!("-{}", fmt_time(counter)), 2)?
+            }
+            _ => (),
+        };
+        Ok(())
+    }
+
+    fn show_timer_end(&mut self) -> Result<()> {
+        clear(self.get_mut_stdout())?;
+        show_message_red(self.get_mut_stdout(), "Timer has ended", 0)?;
+        show_message(
+            self.get_mut_stdout(),
+            "[Q]: Quit, [Space]: Toggle excess counter",
+            1,
+        )?;
+        self.show_extended_time()?;
+
         Ok(())
     }
 }
@@ -55,29 +107,50 @@ impl Counter for Timer {
     }
 
     fn counter(&self) -> u64 {
-        if self.is_running() {
-            let elapsed = self.started.elapsed().as_secs();
-            if self.counter > elapsed {
-                self.counter - elapsed
-            } else {
-                0
-            }
-        } else {
-            self.counter
+        match self.status {
+            Status::Running => self.counter_now(),
+            Status::TimerEnd => self.counter + self.started.elapsed().as_secs(),
+            _ => self.counter,
         }
     }
 
     fn pause(&mut self) {
-        if self.is_running() {
-            self.counter = self.counter();
-            self.status = Status::Paused;
+        match self.status {
+            Status::Running => {
+                self.counter = self.counter_now();
+                self.status = Status::Paused;
+            }
+            Status::TimerEnd => {
+                self.counter = self.counter();
+                self.status = Status::TimerEndPaused;
+            }
+            _ => (),
         }
     }
 
     fn resume(&mut self) {
-        if self.is_paused() {
-            self.status = Status::Running;
-            self.started = Instant::now();
+        match self.status {
+            Status::Paused => {
+                self.status = Status::Running;
+                self.started = Instant::now();
+            }
+            Status::TimerEndPaused => {
+                self.status = Status::TimerEnd;
+                self.started = Instant::now();
+            }
+            _ => (),
+        }
+    }
+
+    fn toggle(&mut self) {
+        match self.status {
+            Status::Paused | Status::TimerEndPaused => {
+                self.resume();
+            }
+            Status::Running | Status::TimerEnd => {
+                self.pause();
+            }
+            _ => (),
         }
     }
 
@@ -108,16 +181,23 @@ impl Counter for Timer {
             _ => (),
         }
 
-        if self.counter() == 0 {
-            self.end_count();
-            self.alert()?;
+        if self.is_running() && self.counter_now() == 0 {
+            self.end_timer();
+            self.alert();
             return Ok(());
         }
 
-        let running = self.is_running();
-        let counter = self.counter();
-        show_view(&mut self.stdout_raw.stdout, counter, running)?;
-
+        match self.status {
+            Status::Running | Status::Paused => {
+                let running = self.is_running();
+                let counter = self.counter();
+                show_view(&mut self.stdout_raw.stdout, counter, running)?;
+            }
+            Status::TimerEnd | Status::TimerEndPaused => {
+                self.show_timer_end()?;
+            }
+            _ => (),
+        }
         Ok(())
     }
 }
