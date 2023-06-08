ColorCookie-dev/porsmo

commit 5013e8f111121d58e1eb26c5661a3ee5757f208b

Revert "Reset feature added, kbd ctrl-r"
This reverts commit a16603f8e326834a000a44c053d60c133bdf8d7d.

Revert "Added showing and parsing millis"

This reverts commit b0b6713a832d760de67a3a7a065d14f1c85d9026.

Revert "New macro for easier raw terminal output"

This reverts commit 2de20340329bfc28a3758dd91302b072a7784337.

Revert "Gui - added more buttons, key events"

This reverts commit 788155ccfc8b4e3d06e762ba6a26f129467d6915.

Revert "A lot of refactoring and GUI project"

This reverts commit d356964f531e02b4db2e9991603b6377b0932fb0.

src/porsmo/pomodoro.rs
@@ -1,0 +1,183 @@
+use crate::{
+    alert::alert,
+    format::fmt_time,
+    input::{listen_for_inputs, Command},
+    terminal::{clear, show_prompt},
+    terminal::{show_counter, TermRawMode},
+};
+use anyhow::Result;
+use porsmo::{
+    counter::Counter,
+    pomodoro::{Mode, Pomodoro},
+    stopwatch::Stopwatch,
+};
+use std::{fmt::Display, io::Write, sync::mpsc::Receiver, thread, time::Duration};
+use termion::color;
+
+pub fn pomodoro(work_time: u64, break_time: u64, long_break_time: u64) -> Result<u64> {
+    let mut pomo = Pomodoro::new(work_time, break_time, long_break_time);
+    let stdout = &mut TermRawMode::new().stdout;
+    let rx = listen_for_inputs();
+
+    loop {
+        match rx.try_recv() {
+            Ok(Command::Quit) => {
+                break;
+            }
+
+            Ok(Command::Pause) => {
+                pomo.pause();
+            }
+
+            Ok(Command::Resume) => {
+                pomo.resume();
+            }
+
+            Ok(Command::Toggle) | Ok(Command::Enter) => {
+                pomo.toggle();
+            }
+
+            Ok(Command::Skip) => {
+                pomo.pause();
+                if skip_prompt(stdout, &rx, pomo.check_next_mode(), pomo.session())? {
+                    pomo.next_mode()
+                } else {
+                    pomo.resume();
+                }
+            }
+
+            _ => (),
+        }
+
+        if pomo.has_ended() {
+            alert_pomo(pomo.check_next_mode());
+            let (counter, next) =
+                start_excess_counting(stdout, &rx, pomo.check_next_mode(), pomo.session())?;
+            if next {
+                pomo.next_mode();
+            } else {
+                return Ok(counter);
+            }
+        }
+
+        let title = match pomo.mode() {
+            Mode::Work => "Pomodoro (Work)",
+            Mode::Break => "Pomodoro (Break)",
+            Mode::LongBreak => "Pomodor (Long Break)",
+        };
+
+        show_counter(
+            stdout,
+            title,
+            fmt_time(pomo.counter()),
+            pomo.is_running(),
+            "[Q]: quit, [Space]: pause/resume.",
+            format!("Round: {}", pomo.session()),
+        )?;
+
+        thread::sleep(Duration::from_millis(100));
+    }
+
+    Ok(pomo.counter())
+}
+
+fn skip_prompt(
+    stdout: &mut impl Write,
+    rx: &Receiver<Command>,
+    next_mode: Mode,
+    session: u64,
+) -> Result<bool> {
+    loop {
+        match rx.try_recv() {
+            Ok(Command::Quit) | Ok(Command::No) => {
+                return Ok(false);
+            }
+
+            Ok(Command::Yes) | Ok(Command::Enter) => {
+                return Ok(true);
+            }
+
+            _ => (),
+        }
+
+        show_prompt_pomo(stdout, next_mode, format!("Round: {}", session))?;
+
+        thread::sleep(Duration::from_millis(100));
+    }
+}
+
+fn start_excess_counting(
+    stdout: &mut impl Write,
+    rx: &Receiver<Command>,
+    next_mode: Mode,
+    session: u64,
+) -> Result<(u64, bool)> {
+    let mut st = Stopwatch::new(0);
+
+    loop {
+        match rx.try_recv() {
+            Ok(Command::Quit) => {
+                st.end_count();
+                break;
+            }
+
+            Ok(Command::Pause) => {
+                st.pause();
+            }
+
+            Ok(Command::Resume) => {
+                st.resume();
+            }
+
+            Ok(Command::Toggle) => {
+                st.toggle();
+            }
+
+            Ok(Command::Enter) => return Ok((st.counter(), true)),
+
+            _ => (),
+        }
+
+        let title = match next_mode {
+            Mode::Work => "Break has ended! Start work?",
+            Mode::Break => "Work has ended! Start break?",
+            Mode::LongBreak => "Work has ended! Start a long break",
+        };
+
+        show_counter(
+            stdout,
+            title,
+            format!("+{}", fmt_time(st.counter())),
+            st.is_running(),
+            "[Q]: Quit, [Enter]: Start, [Space]: toggle",
+            format!("Round: {}", session),
+        )?;
+
+        thread::sleep(Duration::from_millis(100));
+    }
+
+    Ok((st.counter(), false))
+}
+
+pub fn alert_pomo(next_mode: Mode) {
+    let (heading, message) = match next_mode {
+        Mode::Work => ("Your break ended!", "Time for some work"),
+        Mode::Break => ("Pomodoro ended!", "Time for a short break"),
+        Mode::LongBreak => ("Pomodoro 4 sessions complete!", "Time for a long break"),
+    };
+
+    alert(heading.into(), message.into());
+}
+
+pub fn show_prompt_pomo(
+    stdout: &mut impl Write,
+    next_mode: Mode,
+    message: impl Display,
+) -> Result<()> {
+    clear(stdout)?;
+    match next_mode {
+        Mode::Work => show_prompt(stdout, "skip to work?", color::Red, message),
+        Mode::Break => show_prompt(stdout, "skip to break?", color::Green, message),
+        Mode::LongBreak => show_prompt(stdout, "skip to long break?", color::Green, message),
+    }
+}

src/porsmo/stopwatch.rs
@@ -1,0 +1,65 @@
+use crate::{
+    format::fmt_time,
+    input::{listen_for_inputs, Command},
+    terminal::{show_counter, TermRawMode},
+};
+use anyhow::Result;
+use porsmo::{counter::Counter, stopwatch::Stopwatch};
+use std::{io::Write, sync::mpsc::Receiver, thread, time::Duration};
+
+pub fn default_stopwatch_loop<T>(
+    stdout: &mut T,
+    rx: &Receiver<Command>,
+    time: u64,
+    update: impl Fn(&mut T, &Stopwatch) -> Result<()>,
+) -> Result<u64>
+where
+    T: Write,
+{
+    let mut st = Stopwatch::new(time);
+
+    loop {
+        match rx.try_recv() {
+            Ok(Command::Quit) => {
+                st.end_count();
+                break;
+            }
+
+            Ok(Command::Pause) => {
+                st.pause();
+            }
+
+            Ok(Command::Resume) => {
+                st.resume();
+            }
+
+            Ok(Command::Toggle) | Ok(Command::Enter) => {
+                st.toggle();
+            }
+
+            _ => (),
+        }
+
+        update(stdout, &st)?;
+
+        thread::sleep(Duration::from_millis(100));
+    }
+
+    Ok(st.counter())
+}
+
+pub fn stopwatch(time: u64) -> Result<u64> {
+    let mut stdout = &mut TermRawMode::new().stdout;
+    let rx = listen_for_inputs();
+
+    default_stopwatch_loop(&mut stdout, &rx, time, move |stdout, st| {
+        show_counter(
+            stdout,
+            "StopWatch",
+            fmt_time(st.counter()),
+            st.is_running(),
+            "[Q]: quit, [Space]: pause/resume",
+            "",
+        )
+    })
+}

src/porsmo/timer.rs
@@ -1,0 +1,76 @@
+use crate::{
+    alert::alert,
+    format::fmt_time,
+    input::{listen_for_inputs, Command},
+    stopwatch::default_stopwatch_loop,
+    terminal::{show_counter, TermRawMode},
+};
+use anyhow::Result;
+use porsmo::{counter::Counter, timer::Timer};
+use std::{io::Write, sync::mpsc::Receiver, thread, time::Duration};
+
+pub fn timer(time: u64) -> Result<u64> {
+    let mut c = Timer::new(time);
+    let mut stdout = &mut TermRawMode::new().stdout;
+    let rx = listen_for_inputs();
+    let counter_ended_at;
+
+    loop {
+        match rx.try_recv() {
+            Ok(Command::Quit) => {
+                c.end_count();
+                counter_ended_at = c.counter();
+                break;
+            }
+
+            Ok(Command::Pause) => {
+                c.pause();
+            }
+
+            Ok(Command::Resume) => {
+                c.resume();
+            }
+
+            Ok(Command::Toggle) | Ok(Command::Enter) => {
+                c.toggle();
+            }
+
+            _ => (),
+        }
+
+        if c.has_ended() {
+            c.end_count();
+            alert("Timer ended!".into(), "You Porsmo timer has ended".into());
+            counter_ended_at = start_excess_counting(stdout, &rx)?;
+            break;
+        }
+
+        show_counter(
+            &mut stdout,
+            "Timer",
+            fmt_time(c.counter()),
+            c.is_running(),
+            "[Q]: quit, [Space]: pause/resume",
+            "",
+        )?;
+
+        thread::sleep(Duration::from_millis(100));
+    }
+
+    Ok(counter_ended_at)
+}
+
+fn start_excess_counting(stdout: &mut impl Write, rx: &Receiver<Command>) -> Result<u64> {
+    default_stopwatch_loop(stdout, rx, 0, move |stdout, st| {
+        show_counter(
+            stdout,
+            "Timer has ended",
+            format!("+{}", fmt_time(st.counter())),
+            st.is_running(),
+            "[Q]: quit, [Space]: pause/resume",
+            "",
+        )?;
+
+        Ok(())
+    })
+}

porsmo-term/src/pomodoro.rs
@@ -1,126 +1,0 @@
-use crate::{
-    input::{listen_command, Command},
-    program_tick_duration, writeraw,
-};
-use anyhow::Result;
-use porsmo::pomodoro::*;
-use porsmo_helpers::{alert_pomo, fmt_time};
-use std::{
-    io::{stdout, Write},
-    sync::mpsc::Receiver,
-    thread,
-    time::Duration,
-};
-use termion::{color, raw::IntoRawMode};
-
-pub fn pomodoro(work: Duration, rest: Duration, long_rest: Duration) -> Result<()> {
-    let mut counter = Pomodoro::new(work, rest, long_rest, alert_pomo);
-
-    {
-        let mut stdout = stdout().into_raw_mode()?;
-        let rx = listen_command();
-
-        loop {
-            let (title, count, control) = match counter.checked_counter_at() {
-                CountType::Count(count) => (
-                    get_pomo_title(*counter.mode()),
-                    fmt_time(count),
-                    "[Q]: Quit, [Space]: pause/resume",
-                ),
-                CountType::Exceed(count) => (
-                    get_rest_title(*counter.mode()),
-                    format!("+{}", fmt_time(count)),
-                    "[Q]: Quit, [Space]: pause/resume, [Enter]: next session",
-                ),
-            };
-
-            writeraw! {
-                stdout, clear,
-                %text title, color color::Magenta, (1, 1)%,
-                %text count, runcolor counter.is_running(),(1, 2)%,
-                %text control, color color::LightYellow, (1, 3)%,
-                %text format_args!("Round: {}", counter.session()), (1, 4)%,
-            }
-
-            stdout.flush()?;
-
-            match rx.try_recv() {
-                Ok(Command::Quit) => break,
-                Ok(Command::Skip) => {
-                    counter.pause();
-                    if skip_prompt(&mut stdout, &rx, &mut counter)? {
-                        break;
-                    }
-                    counter.resume();
-                }
-                Ok(Command::Enter) if counter.has_ended() => counter.next_mode(),
-                Ok(Command::Enter) | Ok(Command::Space) => counter.toggle(),
-                Ok(Command::Reset) => counter.reset(),
-                _ => (),
-            }
-
-            thread::sleep(program_tick_duration!());
-        }
-    }
-
-    println!();
-    Ok(())
-}
-
-fn skip_prompt(
-    stdout: &mut impl Write,
-    rx: &Receiver<Command>,
-    counter: &mut Pomodoro,
-) -> Result<bool> {
-    loop {
-        view_skip_prompt(stdout, counter.check_next_mode(), counter.session())?;
-        match rx.try_recv() {
-            Ok(Command::Quit) => return Ok(true),
-            Ok(Command::Yes) | Ok(Command::Enter) | Ok(Command::Space) => {
-                counter.next_mode();
-                return Ok(false);
-            }
-            _ => (),
-        }
-        thread::sleep(Duration::from_millis(100));
-    }
-}
-
-fn view_skip_prompt(stdout: &mut impl Write, next_mode: Mode, session: u64) -> Result<()> {
-    let prompt = get_skip_prompt(next_mode);
-
-    writeraw! {
-        stdout, clear,
-        %text prompt, (1, 1)%,
-        %text "[Q]: Quit, [Y]: Yes, [N]: No", color color::LightYellow, (1, 2)%,
-        %text format_args!("Round: {}", session), (1, 3)%,
-    }
-
-    stdout.flush()?;
-
-    Ok(())
-}
-
-fn get_pomo_title(mode: Mode) -> &'static str {
-    match mode {
-        Mode::Work => "pomodoro (work)",
-        Mode::Rest => "pomodoro (break)",
-        Mode::LongRest => "pomodoro (long break)",
-    }
-}
-
-fn get_rest_title(mode: Mode) -> &'static str {
-    match mode {
-        Mode::Work => "Time for some break!",
-        Mode::Rest => "Time to get back to work",
-        Mode::LongRest => "Your break has ended!",
-    }
-}
-
-fn get_skip_prompt(next_mode: Mode) -> String {
-    match next_mode {
-        Mode::Work => format!("{}Skip to work?", color::Fg(color::Red)),
-        Mode::Rest => format!("{}Skip to break?", color::Fg(color::Green)),
-        Mode::LongRest => format!("{}Skip to a long break?", color::Fg(color::Green)),
-    }
-}

porsmo-term/src/stopwatch.rs
@@ -1,48 +1,0 @@
-use crate::{
-    input::{listen_command, Command},
-    program_tick_duration, writeraw,
-};
-use anyhow::Result;
-use porsmo::{counter::*, stopwatch::Stopwatch};
-use porsmo_helpers::fmt_time;
-use std::{
-    io::{stdout, Write},
-    thread,
-    time::Duration,
-};
-use termion::{color, raw::IntoRawMode};
-
-pub fn stopwatch(time: Duration) -> Result<()> {
-    let mut counter = Stopwatch::new(time);
-
-    {
-        let mut stdout = stdout().into_raw_mode()?;
-        let rx = listen_command();
-
-        loop {
-            writeraw! {
-                stdout, clear,
-                %text "Stopwatch", color color::Magenta, (1, 1)%,
-                %text fmt_time(counter.counter_at()),
-                    runcolor counter.is_running(), (1, 2)%,
-                %text "[Q]: Quit, [Space]: pause/resume", color color::LightYellow, (1, 3)%
-            }
-
-            stdout.flush()?;
-
-            match rx.try_recv() {
-                Ok(Command::Quit) => break,
-                Ok(Command::Space) | Ok(Command::Enter) => counter.toggle(),
-                Ok(Command::Reset) => counter.reset(),
-                _ => (),
-            }
-
-            thread::sleep(program_tick_duration!());
-        }
-    }
-
-    println!();
-    // println!("+{}", fmt_time(counter.counter_at().as_secs()));
-
-    Ok(())
-}

porsmo-term/src/timer.rs
@@ -1,65 +1,0 @@
-use crate::{
-    input::{listen_command, Command},
-    program_tick_duration, writeraw,
-};
-use anyhow::Result;
-use porsmo::{counter::*, timer::*};
-use porsmo_helpers::{alert, fmt_time};
-use std::{
-    io::{stdout, Write},
-    thread,
-    time::Duration,
-};
-
-use termion::{color, raw::IntoRawMode};
-
-pub fn timer(time: Duration) -> Result<()> {
-    let mut counter = Timer::new_alert_timer(time, move || {
-        alert("Timer ended".into(), "Your timer has ended".into());
-    });
-
-    {
-        let mut stdout = stdout().into_raw_mode()?;
-        let rx = listen_command();
-
-        loop {
-            writeraw!(stdout, clear);
-            match counter.checked_counter_at() {
-                CountType::Count(c) => {
-                    writeraw! {
-                        stdout,
-                        %text "Timer", color color::Magenta, (1, 1)%,
-                        %text fmt_time(c), runcolor counter.is_running(), (1, 2)%,
-                    }
-                }
-                CountType::Exceed(c) => {
-                    writeraw! {
-                        stdout,
-                        %text "Timer ended!", color color::Magenta, (1, 1)%,
-                        %text format_args!("+{}", fmt_time(c)),
-                            runcolor counter.is_running(), (1, 2)%,
-                    }
-                }
-            }
-
-            writeraw!(
-                stdout,
-                %text "[Q]: Quit, [Space]: pause/resume", color color::LightYellow, (1, 3)%
-            );
-
-            stdout.flush()?;
-
-            match rx.try_recv() {
-                Ok(Command::Quit) => break,
-                Ok(Command::Space) | Ok(Command::Enter) => counter.toggle(),
-                Ok(Command::Reset) => counter.reset(),
-                _ => (),
-            }
-
-            thread::sleep(program_tick_duration!());
-        }
-    }
-
-    println!();
-    Ok(())
-}

porsmo/src/pomodoro.rs
@@ -1,162 +1,0 @@
-pub use crate::{counter::*, timer::*};
-use std::time::Duration;
-
-pub type CountType = crate::timer::CountType<Duration>;
-
-#[derive(Copy, Clone, Debug)]
-pub enum Mode {
-    Work,
-    Rest,
-    LongRest,
-}
-
-#[derive(Debug)]
-pub struct PomoSettings {
-    work: Duration,
-    rest: Duration,
-    long_rest: Duration,
-}
-
-impl Default for PomoSettings {
-    fn default() -> Self {
-        Self {
-            work: Duration::from_secs(25 * 60),
-            rest: Duration::from_secs(5 * 60),
-            long_rest: Duration::from_secs(10 * 60),
-        }
-    }
-}
-
-impl PomoSettings {
-    pub fn new(work: Duration, rest: Duration, long_rest: Duration) -> Self {
-        Self {
-            work,
-            rest,
-            long_rest,
-        }
-    }
-}
-
-pub struct Pomodoro {
-    counter: AlertTimer,
-    session: u64,
-    mode: Mode,
-    options: PomoSettings,
-    alert: fn(Mode),
-}
-
-impl Pomodoro {
-    pub fn new(work: Duration, rest: Duration, long_rest: Duration, alert: fn(Mode)) -> Self {
-        let options = PomoSettings::new(work, rest, long_rest);
-
-        Self {
-            counter: Timer::new_alert_timer(options.work, move || {
-                alert(Mode::Rest);
-            }),
-            session: 0,
-            mode: Mode::Work,
-            options,
-            alert,
-        }
-    }
-}
-
-impl CheckedCount<Duration> for Pomodoro {
-    fn checked_counter_at(&self) -> CountType {
-        self.counter.checked_counter_at()
-    }
-}
-
-impl Pausable for Pomodoro {
-    fn is_running(&self) -> bool {
-        self.counter.is_running()
-    }
-
-    fn is_paused(&self) -> bool {
-        self.counter.is_paused()
-    }
-
-    fn pause(&mut self) {
-        self.counter.pause();
-    }
-
-    fn resume(&mut self) {
-        self.counter.resume();
-    }
-
-    fn toggle(&mut self) {
-        self.counter.toggle();
-    }
-}
-
-impl Reset for Pomodoro {
-    fn reset(&mut self) {
-        self.counter.reset();
-    }
-}
-
-impl Counter<Duration> for Pomodoro {
-    fn counter_at(&self) -> Duration {
-        self.counter.counter_at()
-    }
-}
-
-impl HasEnd for Pomodoro {
-    fn has_ended(&self) -> bool {
-        self.counter.has_ended()
-    }
-}
-
-pub trait PomoMode {
-    fn mode(&self) -> &Mode;
-    fn session(&self) -> u64;
-    fn check_next_mode(&self) -> Mode;
-    fn next_mode(&mut self);
-}
-
-impl PomoMode for Pomodoro {
-    fn mode(&self) -> &Mode {
-        &self.mode
-    }
-
-    fn session(&self) -> u64 {
-        self.session
-    }
-
-    fn check_next_mode(&self) -> Mode {
-        match self.mode {
-            Mode::Work if leap_session(self.session()) => Mode::LongRest,
-            Mode::Work => Mode::Rest,
-            Mode::Rest | Mode::LongRest => Mode::Work,
-        }
-    }
-
-    fn next_mode(&mut self) {
-        let alert = self.alert.clone();
-
-        let target_time = match self.mode {
-            Mode::Work if leap_session(self.session()) => {
-                self.mode = Mode::LongRest;
-                self.options.long_rest
-            }
-            Mode::Work => {
-                self.mode = Mode::Rest;
-                self.options.rest
-            }
-            Mode::Rest | Mode::LongRest => {
-                self.session += 1;
-                self.mode = Mode::Work;
-                self.options.work
-            }
-        };
-
-        let next = self.check_next_mode();
-        self.counter = Timer::new_alert_timer(target_time, move || {
-            alert(next);
-        })
-    }
-}
-
-fn leap_session(session: u64) -> bool {
-    session % 4 == 0 && session != 0
-}

porsmo/src/stopwatch.rs
@@ -1,3 +1,0 @@
-use crate::counter::*;
-
-pub type Stopwatch = TimeCount;

commit 5ff8e319f4911e121eee0d8797049e2b57245dfd

Removed excessive delay
Bug was introduced by 5013e8f

src/porsmo/pomodoro.rs
@@ -97,7 +97,6 @@ pub fn pomodoro(
             format!("Round: {}", pomo.session()),
         )?;
 
-        thread::sleep(Duration::from_millis(100));
     }
 
     Ok(pomo.elapsed())
@@ -124,7 +123,6 @@ fn skip_prompt(
             }
         }
 
-        thread::sleep(Duration::from_millis(100));
     }
 }
 
@@ -167,7 +165,6 @@ fn start_excess_counting(
             format!("Round: {}", session),
         )?;
 
-        thread::sleep(Duration::from_millis(100));
     }
 
     Ok((st.elapsed(), false))

src/porsmo/stopwatch.rs
@@ -45,7 +45,6 @@ pub fn default_stopwatch_loop(
 
         update(&st)?;
 
-        thread::sleep(Duration::from_millis(100));
     }
 
     Ok(st.elapsed())

src/porsmo/timer.rs
@@ -60,7 +60,6 @@ pub fn timer(time: Duration) -> Result<Duration> {
             "",
         )?;
 
-        thread::sleep(Duration::from_millis(100));
     }
 
     Ok(counter_ended_at)