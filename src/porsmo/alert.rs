f34b74aab11c5d9f683391d57620bfb8ee702293
Color Cookie <colorcookie@protonmail.com>
2023-07-05 12:43:26 +0530

    Added Automatic alert mechanism which alerts once
    It relies on RefCell to track if it has already alerted once even with a
    borrowed reference

--- src/porsmo/alert.rs
+++ src/porsmo/alert.rs
@@ -1,7 +1,36 @@
 use crate::prelude::*;
 use rodio::{Decoder, OutputStream, Sink};
-use std::{io::Cursor, thread};
+use std::{io::Cursor, thread, cell::RefCell};
 use notify_rust::Notification;
+
+pub struct Alert {
+    alerted: RefCell<bool>,
+}
+
+impl Default for Alert {
+    fn default() -> Self {
+        Self { alerted: RefCell::new(false) }
+    }
+}
+
+impl Alert {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn alert(&self, title: impl Into<String>, message: impl Into<String>) {
+        if !*self.alerted.borrow() {
+            alert(title, message);
+            self.alerted.replace(true);
+        }
+    }
+
+    pub fn reset(&self) {
+        if *self.alerted.borrow() {
+            self.alerted.replace(false);
+        }
+    }
+}
 
 pub fn notify_default(title: impl AsRef<str>, message: impl AsRef<str>) -> Result<()> {
     Notification::new()
